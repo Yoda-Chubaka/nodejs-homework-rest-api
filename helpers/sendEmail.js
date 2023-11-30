5050b869316a540be31eeb174c1b2662c1147962
Anastasiia Polishchuk <polishchuck1818@gmail.com>
2023-11-30 20:32:45 +0200

    Update

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -12,28 +12,31 @@ apikey.apiKey = ELASTIC_API_KEY;
  
 const api = new ElasticEmail.EmailsApi();
  
-export const sendEmail = ElasticEmail.EmailMessageData.constructFromObject({
-  Recipients: [
-    new ElasticEmail.EmailRecipient("jatal88433@cumzle.com")
-  ],
-  Content: {
-    Body: [
-      ElasticEmail.BodyPart.constructFromObject({
-        ContentType: "HTML",
-        Content: "<strong>Test email</strong>"
-      })
-    ],
-    Subject: "Test email",
-    From: EMAIL_FROM
-  }
-});
+export const sendEmail = async ({ to, subject, html }) => {
+    const email = ElasticEmail.EmailMessageData.constructFromObject({
+        Recipients: [
+            new ElasticEmail.EmailRecipient("limog87256@bustayes.com")
+        ],
+        Content: {
+            Body: [
+                ElasticEmail.BodyPart.constructFromObject({
+                    ContentType: "HTML",
+                    Content: "<strong>Test email</strong>"
+                })
+            ],
+            Subject: "Test email",
+            From: EMAIL_FROM
+        }
+    });
 
-const callback = function(error, data, response) {
-  if (error) {
-    console.error(error.message);
-  } else {
-    console.log('API called successfully.');
-  }
-};
+    const callback = function (error, data, response) {
+        if (error) {
+            console.error(error.message);
+        } else {
+            console.log('API called successfully.');
+        }
+    };
 
-api.emailsPost(sendEmail, callback);
+    api.emailsPost(sendEmail, callback);
+    return true;
+}
